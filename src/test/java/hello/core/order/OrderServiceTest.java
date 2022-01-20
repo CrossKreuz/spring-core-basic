62a0a31df928b9516391bd5649fb32dbe736e343
Min Hyeok Seol <dat.sci.seol@gmail.com>
2022-01-21 06:04:52 +0900

    Test: OrderServiceTest

---
+++ src/test/java/hello/core/order/OrderServiceTest.java
@@ -0,0 +1,34 @@
+package hello.core.order;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import org.assertj.core.api.Assert;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OrderServiceTest {
+    MemberService memberService = new MemberServiceImpl();
+    OrderService orderService = new OrderServiceImpl();
+    @Test
+    void vip_o() {
+        Member vip = new Member(1L, "vio", Grade.VIP);
+        memberService.join(vip);
+
+        Order order_vip = orderService.createOrder(vip.getId(), "vip_product", 10000);
+        Assertions.assertThat(order_vip.getDiscountPrice()).isEqualTo(1000);
+    }
+
+    @Test
+    void vip_x() {
+        Member basic = new Member(2L, "vix", Grade.BASIC);
+        memberService.join(basic);
+
+        Order order_basic = orderService.createOrder(basic.getId(), "basic_product", 10000);
+        Assertions.assertThat(order_basic.getDiscountPrice()).isEqualTo(0);
+    }
+
+}
